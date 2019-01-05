77adf8b12129dcd32e304e35c0690e2ab23f75ef
Karl Granli <karl@gran.li>
2019-01-05 01:34:11 +0100

    Forberede for data-presentasjon

--- clash-royale-stats-2/script.js
+++ clash-royale-stats-2/script.js
@@ -1,15 +1,16 @@
-testingAPI();
+var userContainer = document.getElementById("current-user");
+var clashData = {};
 
-function testingAPI(){
+var initAPI = function() {
   var key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTc4NywiaWRlbiI6IjQwMjc1Nzc0Mjc0NzQ1MTM5MiIsIm1kIjp7fSwidHMiOjE1Mzg2NjQ3OTQ2Mjd9.0JLa2j04z96C3iwkDcPVO_PP6G3jnyFJ_RQ7OVzQmow";
   var url = "https://api.royaleapi.com/player/JYG8VPL";
-console.log(key);
-  var clashData = JSON.parse(httpGet(url, key));
 
-  console.log(clashData.name);
+  clashData = JSON.parse(httpGet(url, key));
+
+  userContainer.innerHTML = "Navn: <strong>" + clashData.name + "</strong>";
 }
 
-function httpGet(url,key){
+var httpGet = function(url,key){
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.open( "GET", url, false );
   xmlHttp.setRequestHeader("auth",key);
