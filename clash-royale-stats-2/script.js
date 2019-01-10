7f3ef4ddd2d3ab79717fb6db64f7c1142d6c000b
Karl Granli <karl@gran.li>
2019-01-10 10:51:31 +0100

    no message

--- clash-royale-stats-2/script.js
+++ clash-royale-stats-2/script.js
@@ -1,19 +1,46 @@
 var userContainer = document.getElementById("current-user");
+var navButtons = document.getElementsByClassName("nav__item");
 var clashData = {};
 
 var initAPI = function() {
-  var key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTc4NywiaWRlbiI6IjQwMjc1Nzc0Mjc0NzQ1MTM5MiIsIm1kIjp7fSwidHMiOjE1Mzg2NjQ3OTQ2Mjd9.0JLa2j04z96C3iwkDcPVO_PP6G3jnyFJ_RQ7OVzQmow";
-  var url = "https://api.royaleapi.com/player/JYG8VPL";
+    var key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTc4NywiaWRlbiI6IjQwMjc1Nzc0Mjc0NzQ1MTM5MiIsIm1kIjp7fSwidHMiOjE1Mzg2NjQ3OTQ2Mjd9.0JLa2j04z96C3iwkDcPVO_PP6G3jnyFJ_RQ7OVzQmow";
+    var url = "https://api.royaleapi.com/player/JYG8VPL";
 
-  clashData = JSON.parse(httpGet(url, key));
+    clashData = JSON.parse(httpGet(url, key));
 
-  userContainer.innerHTML = "Navn: <strong>" + clashData.name + "</strong>";
-}
+    userContainer.innerHTML = clashData.name;
 
-var httpGet = function(url,key){
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open( "GET", url, false );
-  xmlHttp.setRequestHeader("auth",key);
-  xmlHttp.send(null);
-  return xmlHttp.responseText;
-}
+    detectNavigation();
+};
+
+var httpGet = function(url, key) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", url, false);
+    xmlHttp.setRequestHeader("auth", key);
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+};
+
+var detectNavigation = function() {
+	for (var i = 0; i < navButtons.length; i++) {
+        var elem = navButtons[i];
+        elem.onclick = function() {
+            menuClick(this);
+        };
+    }
+};
+
+var menuClick = function(button) {
+	removeActiveMenuItem();
+    var dataToShow = button.getAttribute("data-switch");
+    button.classList.add("active");
+};
+
+var removeActiveMenuItem = function() {
+	for (var i = 0; i < navButtons.length; i++) {
+        var elem = navButtons[i];
+        elem.classList.remove("active");
+    }
+};
+
+initAPI();
