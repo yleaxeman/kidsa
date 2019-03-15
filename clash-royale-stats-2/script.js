8cd358531647334fb77873d7f19f16b1cc84d55e
Karl Granli <karl@gran.li>
2019-03-15 14:20:44 +0100

    Console.log the data for reference. Ready to start rendering data per category

--- clash-royale-stats-2/script.js
+++ clash-royale-stats-2/script.js
@@ -1,14 +1,17 @@
 var userContainer = document.getElementById("current-user");
 var navButtons = document.getElementsByClassName("nav__item");
 var clashData = {};
+var dataContainer = document.getElementById("clash-data");
 
-var initAPI = function() {
+var initApp = function() {
     var key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTc4NywiaWRlbiI6IjQwMjc1Nzc0Mjc0NzQ1MTM5MiIsIm1kIjp7fSwidHMiOjE1Mzg2NjQ3OTQ2Mjd9.0JLa2j04z96C3iwkDcPVO_PP6G3jnyFJ_RQ7OVzQmow";
     var url = "https://api.royaleapi.com/player/JYG8VPL";
 
     clashData = JSON.parse(httpGet(url, key));
 
     userContainer.innerHTML = clashData.name;
+
+    console.log(clashData);//list data to get a glimpse of what's available
 
     detectNavigation();
 };
@@ -22,7 +25,7 @@ var httpGet = function(url, key) {
 };
 
 var detectNavigation = function() {
-	for (var i = 0; i < navButtons.length; i++) {
+    for (var i = 0; i < navButtons.length; i++) {
         var elem = navButtons[i];
         elem.onclick = function() {
             menuClick(this);
@@ -31,16 +34,21 @@ var detectNavigation = function() {
 };
 
 var menuClick = function(button) {
-	removeActiveMenuItem();
+    removeActiveMenuItem();
     var dataToShow = button.getAttribute("data-switch");
     button.classList.add("active");
+    showData(dataToShow);
 };
 
 var removeActiveMenuItem = function() {
-	for (var i = 0; i < navButtons.length; i++) {
+    for (var i = 0; i < navButtons.length; i++) {
         var elem = navButtons[i];
         elem.classList.remove("active");
     }
 };
 
-initAPI();
+var showData = function(dataName) {
+    var jsonData = clashData[dataName];
+};
+
+initApp();
